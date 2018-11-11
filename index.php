dfa1fb2214c1daf1075f005f5629491ce5660701
celine <celine.minaudier@gmail.com>
2018-11-11 12:47:40 +0100

    modify files

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+include('inc/head.php');
+
+
+if (isset($_POST['contenu'])){
+    $fichier ='files/roswell/'. $_POST['filename'];
+    $file = fopen($fichier, 'w');
+    fwrite($file, $_POST['contenu']);
+    fclose($file);
+}
+//
+//?>
+<!---->
+<?php
+$dir = opendir('files/roswell');
+
+
+while ($file = readdir($dir)){
+    if ($file !='.' && $file !='..'){
+        if (is_dir('files/roswell/' . $file)){
+            $subdir = opendir('files/roswell/' . $file);
+            while($subfiles = readdir($subdir)){
+                if ($subfiles !='.' && $subfiles!='..') {
+                    echo '<a href="?f=' . $subfiles . '"><br/>';
+                    echo $subfiles;
+                    echo '</a>';
+                }
+            }
+        }else{
+            echo '<a href="?f=' . $file .'"><br/>';
+            echo $file;
+            echo '</a>';
+        }
+        echo '<br/>';
+    }
+
+}
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<?php
+if (isset($_GET['f'])){
+
+    $fichier ='files/roswell/'. $_GET['f'];
+    $contenu = file_get_contents($fichier);
+
+?>
+
+<body>
+<h4>Voici le contenu de tes répertoires et fichiers.</h4>
+<div id="id=content">
+    <form action="index.php" method="post">
+        <input type="hidden" name="filename" value="<?php echo $_GET['f']?>">
+        <textarea name="contenu" id="" style="width:100%;height: 200px;"><?php echo $contenu?></textarea>
+        <input type="submit" value="Envoyer">
+    </form>
+</div>
+<?php } ?>
+
+</body>
+</html>
+
+<?php include('inc/foot.php'); ?>
