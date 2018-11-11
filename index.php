37e9ed348951c2b4115cf95787886c1dad6f3189
celine <celine.minaudier@gmail.com>
2018-11-11 15:07:21 +0100

    add delete button

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
 <?php
 
 include('inc/head.php');
 
+if (isset($_GET['f'])) {
+
+    $fichier = $_GET['f'];
+    $contenu = file_get_contents($fichier);
+}
 
 if (isset($_POST['contenu'])){
     $fichier ='files/roswell/'. $_POST['filename'];
@@ -9,14 +24,30 @@ if (isset($_POST['contenu'])){
     fwrite($file, $_POST['contenu']);
     fclose($file);
 }
-//
-//?>
-<!---->
+
+if (isset($_POST['delete'])){
+    unlink($_POST['deleteFile']);
+}
+
+$it = new RecursiveDirectoryIterator("files/roswell");
+
+foreach(new RecursiveIteratorIterator($it) as $file) {
+        if($file->getFilename() != '.' && $file->getFilename() != '..'){
+
+?>
+
+        <a href=index.php?f=<?php echo $file ?> > <?php echo $file ?> </a>
+        <form action="" method="post">
+            <input type="hidden" name="deleteFile" value="<?php echo $file ?>">
+            <input type="submit" name="delete" value="Supprimer">
+        </form>
+
 <?php
-$dir = opendir('files/roswell');
 
+        }
+}
 
-while ($file = readdir($dir)){
+/*while ($file = readdir($dir)){
     if ($file !='.' && $file !='..'){
         if (is_dir('files/roswell/' . $file)){
             $subdir = opendir('files/roswell/' . $file);
@@ -35,24 +66,7 @@ while ($file = readdir($dir)){
         echo '<br/>';
     }
 
-}
-
-
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<?php
-if (isset($_GET['f'])){
-
-    $fichier ='files/roswell/'. $_GET['f'];
-    $contenu = file_get_contents($fichier);
+}*/
 
 ?>
 
@@ -63,9 +77,9 @@ if (isset($_GET['f'])){
         <input type="hidden" name="filename" value="<?php echo $_GET['f']?>">
         <textarea name="contenu" id="" style="width:100%;height: 200px;"><?php echo $contenu?></textarea>
         <input type="submit" value="Envoyer">
+
     </form>
 </div>
-<?php } ?>
 
 </body>
 </html>
